8bc33e08bddf8ce4544bd1baafe173708995970b
MegaHotel <megahotel1000@gmail.com>
2022-07-29 00:17:04 -0400

    imagine reading a commit message

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,6 +1,8 @@
 --[[
 lvim is the global options object
 
+
+
 Linters should be
 filled in as strings with either
 a global executable or a path to
@@ -347,3 +349,127 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- Guifont
 vim.o.guifont = "Fira Code:h11"
+
+
+-- Neovide config
+vim.cmd([[
+if exists("g:neovide")
+    let g:neovide_refresh_rate=180
+    let g:neovide_floating_blur_amount_x = 2.0
+    let g:neovide_floating_blur_amount_y = 2.0
+    let g:neovide_cursor_antialiasing=v:true
+    colorscheme gruvbox-baby
+
+    " Gruvbox colorscheme
+    " setup palette dictionary
+    let s:gb = {}
+
+    " fill it with absolute colors
+    let s:gb.dark0_hard  = ['#1d2021', 234]     " 29-32-33
+    let s:gb.dark0       = ['#282828', 235]     " 40-40-40
+    let s:gb.dark0_soft  = ['#32302f', 236]     " 50-48-47
+    let s:gb.dark1       = ['#3c3836', 237]     " 60-56-54
+    let s:gb.dark2       = ['#504945', 239]     " 80-73-69
+    let s:gb.dark3       = ['#665c54', 241]     " 102-92-84
+    let s:gb.dark4       = ['#7c6f64', 243]     " 124-111-100
+    let s:gb.dark4_256   = ['#7c6f64', 243]     " 124-111-100
+
+    let s:gb.gray_245    = ['#928374', 245]     " 146-131-116
+    let s:gb.gray_244    = ['#928374', 244]     " 146-131-116
+
+    let s:gb.light0_hard = ['#f9f5d7', 230]     " 249-245-215
+    let s:gb.light0      = ['#fbf1c7', 229]     " 253-244-193
+    let s:gb.light0_soft = ['#f2e5bc', 228]     " 242-229-188
+    let s:gb.light1      = ['#ebdbb2', 223]     " 235-219-178
+    let s:gb.light2      = ['#d5c4a1', 250]     " 213-196-161
+    let s:gb.light3      = ['#bdae93', 248]     " 189-174-147
+    let s:gb.light4      = ['#a89984', 246]     " 168-153-132
+    let s:gb.light4_256  = ['#a89984', 246]     " 168-153-132
+
+    let s:gb.bright_red     = ['#fb4934', 167]     " 251-73-52
+    let s:gb.bright_green   = ['#b8bb26', 142]     " 184-187-38
+    let s:gb.bright_yellow  = ['#fabd2f', 214]     " 250-189-47
+    let s:gb.bright_blue    = ['#83a598', 109]     " 131-165-152
+    let s:gb.bright_purple  = ['#d3869b', 175]     " 211-134-155
+    let s:gb.bright_aqua    = ['#8ec07c', 108]     " 142-192-124
+    let s:gb.bright_orange  = ['#fe8019', 208]     " 254-128-25
+
+    let s:gb.neutral_red    = ['#cc241d', 124]     " 204-36-29
+    let s:gb.neutral_green  = ['#98971a', 106]     " 152-151-26
+    let s:gb.neutral_yellow = ['#d79921', 172]     " 215-153-33
+    let s:gb.neutral_blue   = ['#458588', 66]      " 69-133-136
+    let s:gb.neutral_purple = ['#b16286', 132]     " 177-98-134
+    let s:gb.neutral_aqua   = ['#689d6a', 72]      " 104-157-106
+    let s:gb.neutral_orange = ['#d65d0e', 166]     " 214-93-14
+
+    let s:gb.faded_red      = ['#9d0006', 88]      " 157-0-6
+    let s:gb.faded_green    = ['#79740e', 100]     " 121-116-14
+    let s:gb.faded_yellow   = ['#b57614', 136]     " 181-118-20
+    let s:gb.faded_blue     = ['#076678', 24]      " 7-102-120
+    let s:gb.faded_purple   = ['#8f3f71', 96]      " 143-63-113
+    let s:gb.faded_aqua     = ['#427b58', 66]      " 66-123-88
+    let s:gb.faded_orange   = ['#af3a03', 130]     " 175-58-3
+
+
+    let s:bg0  = s:gb.dark0
+    let s:bg0  = s:gb.dark0_soft
+
+    let s:bg1  = s:gb.dark1
+    let s:bg2  = s:gb.dark2
+    let s:bg3  = s:gb.dark3
+    let s:bg4  = s:gb.dark4
+
+    let s:gray = s:gb.gray_245
+
+    let s:fg0 = s:gb.light0
+    let s:fg1 = s:gb.light1
+    let s:fg2 = s:gb.light2
+    let s:fg3 = s:gb.light3
+    let s:fg4 = s:gb.light4
+
+    let s:fg4_256 = s:gb.light4_256
+
+    let s:red    = s:gb.bright_red
+    let s:green  = s:gb.bright_green
+    let s:yellow = s:gb.bright_yellow
+    let s:blue   = s:gb.bright_blue
+    let s:purple = s:gb.bright_purple
+    let s:aqua   = s:gb.bright_aqua
+    let s:orange = s:gb.bright_orange
+    let s:bg0[1]    = 0
+    let s:fg4[1]    = 7
+    let s:gray[1]   = 8
+    let s:red[1]    = 9
+    let s:green[1]  = 10
+    let s:yellow[1] = 11
+    let s:blue[1]   = 12
+    let s:purple[1] = 13
+    let s:aqua[1]   = 14
+    let s:fg1[1]    = 15
+
+    let g:terminal_color_0 = s:bg0[0]
+    let g:terminal_color_8 = s:gray[0]
+
+    let g:terminal_color_1 = s:gb.neutral_red[0]
+    let g:terminal_color_9 = s:red[0]
+
+    let g:terminal_color_2 = s:gb.neutral_green[0]
+    let g:terminal_color_10 = s:green[0]
+
+    let g:terminal_color_3 = s:gb.neutral_yellow[0]
+    let g:terminal_color_11 = s:yellow[0]
+
+    let g:terminal_color_4 = s:gb.neutral_blue[0]
+    let g:terminal_color_12 = s:blue[0]
+
+    let g:terminal_color_5 = s:gb.neutral_purple[0]
+    let g:terminal_color_13 = s:purple[0]
+
+    let g:terminal_color_6 = s:gb.neutral_aqua[0]
+    let g:terminal_color_14 = s:aqua[0]
+
+    let g:terminal_color_7 = s:fg4[0]
+    let g:terminal_color_15 = s:fg1[0]
+
+endif
+]])
