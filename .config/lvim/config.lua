15f43a0117fdcef61ca58c04fb2b217379c457d0
MegaHotel <megahotel1000@gmail.com>
2023-02-28 01:30:50 -0500

    salad

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -15,6 +15,7 @@ lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "gruvbox-baby"
 vim.opt.relativenumber = true
+vim.opt.timeoutlen = 150
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -191,7 +192,6 @@ lvim.builtin.which_key.mappings["m"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 
@@ -489,6 +489,9 @@ lvim.plugins = {
   {
     "p00f/nvim-ts-rainbow",
   },
+  {
+    "rcarriga/nvim-notify",
+  }
 }
 
 -- Enable treesitter-rainbow
@@ -517,7 +520,7 @@ require('nvim-lastplace').setup {
 
 
 -- Guifont
-vim.o.guifont = "Fira Code:h11"
+vim.o.guifont = "Fira Code:h15"
 
 -- Neovide config
 vim.cmd([[
