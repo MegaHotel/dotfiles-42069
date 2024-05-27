9d6541a69eeec9b2fb77cf108f68dc87a0d1c1e4
MegaHotel <megahotel1000@gmail.com>
2024-05-26 20:21:51 -0400

    shutup i3-msg

--- .config/fish/functions/cdv.fish
+++ .config/fish/functions/cdv.fish
@@ -2,5 +2,5 @@ function cdv --description "cd to a folder and open vim"
   cd $argv && \
   vim && \
   sleep 0.1 && \
-  i3-msg '[class="^neovide$"] focus; [class="^neovide$"] move up; [class="^neovide$"] resize grow height 20 px or 20 ppt; [class="^neovide$"] fullscreen toggle;';
+  i3-msg '[class="^neovide$"] focus; [class="^neovide$"] move up; [class="^neovide$"] resize grow height 20 px or 20 ppt; [class="^neovide$"] fullscreen toggle;' > /dev/null 2>&1;
 end
